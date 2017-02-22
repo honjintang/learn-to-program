4d9c26f57b09bee79f69bdc81e324cf15c43ba1f
Vivien <vhjtang@gmail.com>
2017-02-22 12:48:12 +0000

    Added solutions to chapter 6

---
+++ chap06/angry_boss.rb
@@ -0,0 +1,3 @@
+puts "What do you want?"
+your_response = gets.chomp
+puts 'WHADDYA MEAN "I WANT ' + your_response.upcase + '" ?!? YOU\'RE FIRED!!'
---
+++ chap06/table_of_contents.rb
@@ -0,0 +1,6 @@
+line_width = 50
+puts "Table of Contents".center(line_width)
+puts ""
+puts "Chapter 1: Getting started".ljust(line_width/2) + "page 1".center(line_width/2)
+puts "Chapter 2: Numbers".ljust(line_width/2) + "page 9".center(line_width/2)
+puts "Chapter 3: Letters".ljust(line_width/2) + "page 13".center(line_width/2)
